97bef1471134b347f6be134c19b005bb770bb8e0
ikarosouza <ikarolp@hotmail.com>
2014-09-21 23:11:40 -0300

    Create the add contact function

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+	
+	def new
+	end
+
+	def show
+		@contact = Contact.find(params[:id])
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+
+		@contact.save
+		redirect_to @contact
+	end
+
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :phone, :email, :adress)
+		end
+
+end
+
