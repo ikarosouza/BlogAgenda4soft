271028b470bd516c442d5d295384becc211aa9fc
ikarosouza <ikarolp@hotmail.com>
2014-09-26 22:31:38 -0300

    Improve the style with bootstrap

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
-	has_many :notes
+	has_many :notes, dependent: :destroy
 	validates :name, presence: true, length: {minimum: 5}
 	validates :phone, presence: true, length: {minimum: 8}
 end
